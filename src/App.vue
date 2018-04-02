238ac4ecf5f0601b141357163f0910ca679c63c2
Peter Rao <peterrao@Peters-MacBook-Pro.local>
2018-04-02 15:33:00 -0400

    removed default tasks

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,7 @@ export default {
   name: 'App',
   data: function () {
     return {
-      todos: [{
-        title: 'Todo A',
-        project: 'Project A',
-        done: false
-      }, {
-        title: 'Todo B',
-        project: 'Project B',
-        done: true
-      }, {
-        title: 'Todo C',
-        project: 'Project C',
-        done: false
-      }, {
-        title: 'Todo D',
-        project: 'Project D',
-        done: false
-      }]
+      todos: []
     }
   }
 }
